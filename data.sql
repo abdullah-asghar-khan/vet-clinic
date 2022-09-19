993940e943b6ec606fee5fc9d3300f77bd2b94f6
Abdullah2213565 <dulakhan024@gmail.com>
2022-09-19 10:05:16 +0200

    Add 2,5 million into owners table

--- data.sql
+++ data.sql
@@ -78,3 +78,9 @@ INSERT INTO visits (animals_id, vets_id, date_of_visit)
   (SELECT id FROM vets) vets_ids, 
   generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
 
+-- INSERT 2.5million data int owners table
+  insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 
+  'owner_' || generate_series(1,2500000) || '@mail.com';
+
+  insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 
+  'owner_' || generate_series(1,2500000) || '@mail.com';
