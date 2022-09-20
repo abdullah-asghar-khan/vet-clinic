b08d6fca065e76942aa982178530495c002e4679
Abdullah2213565 <dulakhan024@gmail.com>
2022-09-20 09:52:36 +0200

    Queries added

--- queries.sql
+++ queries.sql
@@ -173,3 +173,9 @@ JOIN species ON species.id = animals.species_id
 WHERE vets.name = 'Maisy Smith'
 GROUP BY species.name 
 ORDER BY count DESC LIMIT 1;
+
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animals_id = 4;
+
+EXPLAIN ANALYZE SELECT * FROM visits where vets_id = 2;
+
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
